c0ef6fd1ef08d312f702975b1412edbe83c7b910
otherchris <chriscaragianis@gmail.com>
2020-05-30 00:11:37 -0400

    wip

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule IncomingDialer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :plug_cowboy]
+      extra_applications: [:logger, :plug_cowboy],
+      mod: {IncomingDialer.Application, []}
     ]
   end
 
